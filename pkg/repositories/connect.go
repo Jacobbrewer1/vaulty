093c527514d43f45fb124fefd36f86cbabcefb5e
Jacob Brewer <Jacob.brewer@bthree.uk>
2024-10-01 08:35:58 +0100

    feat(init): Initial commit (#1)
    * Adding base of the package
    
    * Adding vendor
    
    * Adding makefile
    
    * Adding quired deps
    
    * Updating the vaulty package name
    
    * vendor
    
    * vaulty name

---
+++ pkg/repositories/connect.go
@@ -0,0 +1,132 @@
+package repositories
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log/slog"
+	"os"
+	"time"
+
+	"github.com/Jacobbrewer1/vaulty/pkg/logging"
+	"github.com/Jacobbrewer1/vaulty/pkg/vaulty"
+	_ "github.com/go-sql-driver/mysql"
+	vault2 "github.com/hashicorp/vault/api"
+	"github.com/jmoiron/sqlx"
+	"github.com/spf13/viper"
+)
+
+type DatabaseConnector interface {
+	ConnectDB() (*Database, error)
+}
+
+type databaseConnector struct {
+	ctx            context.Context
+	client         vaulty.Client
+	vip            *viper.Viper
+	currentSecrets *vault2.Secret
+}
+
+func NewDatabaseConnector(opts ...ConnectionOption) (DatabaseConnector, error) {
+	c := new(databaseConnector)
+
+	for _, opt := range opts {
+		opt(c)
+	}
+
+	if c.ctx == nil {
+		c.ctx = context.Background()
+	}
+
+	if c.client == nil {
+		return nil, errors.New("no vault client provided")
+	} else if c.vip == nil {
+		return nil, errors.New("no viper configuration provided")
+	} else if c.currentSecrets == nil {
+		return nil, errors.New("no current secrets provided")
+	}
+
+	return c, nil
+}
+
+// ConnectDB connects to the database
+func (d *databaseConnector) ConnectDB() (*Database, error) {
+	if !d.vip.IsSet("vault") {
+		return nil, errors.New("no vault configuration found")
+	}
+
+	d.vip.Set("database.connection_string", generateConnectionStr(d.vip, d.currentSecrets))
+	sqlxDb, err := createConnection(d.vip)
+	if err != nil {
+		return nil, fmt.Errorf("error connecting to database: %w", err)
+	}
+
+	db := NewDatabase(sqlxDb)
+
+	go func() {
+		err := vaulty.RenewLease(d.ctx, d.client, d.vip.GetString("vault.database.path"), d.currentSecrets, func() (*vault2.Secret, error) {
+			slog.Warn("Vault lease expired, reconnecting to database")
+
+			vs, err := d.client.GetSecret(d.ctx, d.vip.GetString("vault.database.path"))
+			if err != nil {
+				return nil, fmt.Errorf("error getting secrets from vault: %w", err)
+			}
+
+			dbConnectionString := generateConnectionStr(d.vip, vs)
+			d.vip.Set("database.connection_string", dbConnectionString)
+
+			newDb, err := createConnection(d.vip)
+			if err != nil {
+				return nil, fmt.Errorf("error connecting to database: %w", err)
+			}
+
+			if err := db.Reconnect(d.ctx, newDb); err != nil {
+				return nil, fmt.Errorf("error reconnecting to database: %w", err)
+			}
+
+			slog.Info("Database reconnected")
+
+			return vs, nil
+		})
+		if err != nil {
+			slog.Error("Error renewing vault lease", slog.String(logging.KeyError, err.Error()))
+			os.Exit(1) // Forces new credentials to be fetched
+		}
+	}()
+
+	slog.Info("Database connection established with vault")
+	return db, nil
+}
+
+func createConnection(v *viper.Viper) (*sqlx.DB, error) {
+	connectionString := v.GetString("database.connection_string")
+	if connectionString == "" {
+		return nil, errors.New("no database connection string provided")
+	}
+
+	db, err := sqlx.Open("mysql", connectionString)
+	if err != nil {
+		return nil, fmt.Errorf("error connecting to database: %w", err)
+	}
+
+	// Test the connection.
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := db.PingContext(ctx); err != nil {
+		return nil, fmt.Errorf("error connecting to database: %w", err)
+	}
+
+	slog.Info("Connected to database")
+
+	return db, nil
+}
+
+func generateConnectionStr(v *viper.Viper, vs *vault2.Secret) string {
+	return fmt.Sprintf("%s:%s@tcp(%s)/%s?timeout=90s&multiStatements=true&parseTime=true",
+		vs.Data["username"],
+		vs.Data["password"],
+		v.GetString("database.host"),
+		v.GetString("database.schema"),
+	)
+}
---
+++ pkg/repositories/database_test.go
@@ -0,0 +1,56 @@
+package repositories
+
+import (
+	"context"
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/jmoiron/sqlx"
+	"github.com/stretchr/testify/suite"
+)
+
+type databaseSuite struct {
+	suite.Suite
+
+	mockDb sqlmock.Sqlmock
+	sqlxDb *sqlx.DB
+	db     *Database
+
+	ctx context.Context
+}
+
+func TestDatabaseSuite(t *testing.T) {
+	suite.Run(t, new(databaseSuite))
+}
+
+func (s *databaseSuite) SetupTest() {
+	db, mock, err := sqlmock.New()
+	s.NoError(err)
+	s.mockDb = mock
+	s.sqlxDb = sqlx.NewDb(db, "sqlmock")
+	s.db = NewDatabase(s.sqlxDb)
+	s.ctx = context.Background()
+}
+
+func (s *databaseSuite) AfterTest(suiteName, testName string) {
+	s.NoError(s.mockDb.ExpectationsWereMet())
+}
+
+func (s *databaseSuite) TearDownTest() {
+	s.mockDb = nil
+	s.NoError(s.sqlxDb.Close())
+	s.sqlxDb = nil
+	s.db = nil
+}
+
+func (s *databaseSuite) TestReconnect() {
+	newDb := sqlx.NewDb(s.sqlxDb.DB, "sqlmocknew")
+
+	s.mockDb.ExpectClose()
+	s.mockDb.ExpectPing()
+	err := s.db.Reconnect(s.ctx, newDb)
+	s.NoError(err)
+
+	// Ensure the new database connection is replaced.
+	s.Equal(newDb, s.db.DB)
+}
