f3e6134d048df61aa7530dab31af989acba894c3
Jacob Brewer <Jacob.brewer@bthree.uk>
2025-02-06 09:21:48 +0000

    gen

--- mock_ClientHandler.go
+++ mock_ClientHandler.go
@@ -12,7 +12,7 @@ type MockClientHandler struct {
 	mock.Mock
 }
 
-// Client provides a mock function with given fields:
+// Client provides a mock function with no fields
 func (_m *MockClientHandler) Client() *api.Client {
 	ret := _m.Called()
 
